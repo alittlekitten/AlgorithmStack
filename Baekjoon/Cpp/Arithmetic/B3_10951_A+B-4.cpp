25b3bd2ef4f909ec023898536e10302ea5cbb6dd
Ssoco <dlscjs8646@gmail.com>
2021-09-12 22:13:13 +0900

    Update B3_10951_A+B-4.cpp

--- Baekjoon/Cpp/Arithmetic/B3_10951_A+B-4.cpp
+++ Baekjoon/Cpp/Arithmetic/B3_10951_A+B-4.cpp
@@ -2,11 +2,8 @@
 
 using namespace std;
 
-int main(void)
-{
-    int a = 0;
-    int b = 0;
-
-    while(cin >> a >> b) // EOF가 오면 while문 정지
-        cout <<  a+b  << endl;
+int main(void) {
+    int a, b;
+    while (cin >> a >> b) // EOF가 오면 while문 정지
+        cout << a + b << endl;
 }
