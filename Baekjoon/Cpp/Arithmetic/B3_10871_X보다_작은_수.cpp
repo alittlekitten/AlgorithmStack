28a14dcd4094e14ad2c7c42169e0398d21eeae7c
Ssoco <dlscjs8646@gmail.com>
2021-09-14 00:53:52 +0900

    Create B3_10871_X보다_작은_수.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B3_10871_X보다_작은_수.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	int n, x, a;
+	cin >> n >> x;
+	for (int i = 0; i < n; ++i) {
+		cin >> a;
+		if (a < x) cout << a << " ";
+	}
+
+	return 0;
+}
