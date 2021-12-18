88f8e6ec9be28ef97ff1054b4207959a9393b6ea
Ssoco <dlscjs8646@gmail.com>
2021-12-19 02:50:26 +0900

    Create S1_1697_숨바꼭질.cpp

---
+++ Baekjoon/Cpp/BFS/S1_1697_숨바꼭질.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	// BFS
+
+	int n, k;
+	cin >> n >> k;
+
+	// case 1. 수빈 >= 동생 -> 무조건 -1씩 이동해서 동생을 찾아야 한다. 아님 같은 위치에 있든가!
+	if (n >= k) cout << n - k;
+
+	// case 2. 수빈 < 동생 -> 동생 위치를 /2 해가면서 수빈이랑 가장 가까운 위치를 찾는다. 두개가 같은 만큼 차이가 난다면 /2를 덜한 쪽을 선택한다.
+	else {
+		bool chk[100001] = { false, };
+		chk[n] = true;
+		queue<pair<int, int>> q;
+		q.push(make_pair(n, 0));
+		while (!q.empty()) {
+			int first = q.front().first;
+			int second = q.front().second;
+			q.pop();
+			if (first == k) {
+				cout << second;
+				break;
+			}
+
+			if (first + 1 <= 100000 && !chk[first + 1]) {
+				chk[first + 1] = true;
+				q.push(make_pair(first + 1, second + 1));
+			}
+			if (first - 1 >= 0 && !chk[first - 1]) {
+				q.push(make_pair(first - 1, second + 1));
+				chk[first - 1] = true;
+			}
+			if (first * 2 <= 100000 && !chk[first * 2]) {
+				q.push(make_pair(first * 2, second + 1));
+				chk[first * 2] = true;
+			}
+		}
+	}
+
+	
+	return 0;
+}
