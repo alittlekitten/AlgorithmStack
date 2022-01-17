daccc102c999af8e27930f4894f6cd7616deb5f2
Ssoco <dlscjs8646@gmail.com>
2022-01-17 23:58:44 +0900

    Create S5_2693_N번째_큰_수.cpp

---
+++ Baekjoon/Cpp/Arithmetic/S5_2693_N번째_큰_수.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	// 2693번 N번째 큰 수
+	int t, tmp;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		vector<int> a;
+		for (int j = 0; j < 10; ++j) {
+			cin >> t;
+			a.push_back(t);
+			sort(a.begin(), a.end());
+		}
+		cout << a[8] << '\n';
+		a.clear();
+	}
+	
+	return 0;
+}
