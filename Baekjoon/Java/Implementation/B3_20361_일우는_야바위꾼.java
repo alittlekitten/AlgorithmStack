0173786347f4faf6757c480abeff368368d59af0
Ssoco <dlscjs8646@gmail.com>
2022-02-14 12:40:42 +0900

    Update B3_20361_일우는_야바위꾼.java

--- Baekjoon/Java/Implementation/B3_20361_일우는_야바위꾼.java
+++ Baekjoon/Java/Implementation/B3_20361_일우는_야바위꾼.java
@@ -8,21 +8,21 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-        N = sc.nextInt();
-        X = sc.nextInt();
-        K = sc.nextInt();
+		N = sc.nextInt();
+		X = sc.nextInt();
+		K = sc.nextInt();
 
-        // 정답 위치 미리 할당
-        answer = X;
+		// 정답 위치 미리 할당
+		answer = X;
 
-        // 반복하면서 정답이 있는 위치를 바꾸는 경우 answer를 바꿔줌
-        for (int j = 0; j < K; ++j) {
-            int tmp1 = sc.nextInt();
-            int tmp2 = sc.nextInt();
-            if (tmp1 == answer) answer = tmp2;
-            else if (tmp2 == answer) answer = tmp1;
-        }
+		// 반복하면서 정답이 있는 위치를 바꾸는 경우 answer를 바꿔줌
+		for (int j = 0; j < K; ++j) {
+		    int tmp1 = sc.nextInt();
+		    int tmp2 = sc.nextInt();
+		    if (tmp1 == answer) answer = tmp2;
+		    else if (tmp2 == answer) answer = tmp1;
+		}
 
-        System.out.println(answer);
+		System.out.println(answer);
 	}
 }
