fa24ddaa6952bfa81514f3319604de87d0591314
Ssoco <dlscjs8646@gmail.com>
2022-02-16 15:23:34 +0900

    Create S3_15652_N과_M_4.java

---
+++ Baekjoon/Java/Comb_Perm/S3_15652_N과_M_4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int N, M;
+	static int [] nums;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		nums = new int[N];
+		
+		// 조합
+		comb(0, 1);
+		
+	}
+
+	private static void comb(int cnt, int start) {
+		if (cnt == M){
+			for (int i = 0; i < N; ++i) {
+				if(nums[i] != 0) System.out.print(nums[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = start; i <= N; ++i) {
+			nums[cnt] = i;
+			comb(cnt+1, i);
+		}
+	}
+
+}
