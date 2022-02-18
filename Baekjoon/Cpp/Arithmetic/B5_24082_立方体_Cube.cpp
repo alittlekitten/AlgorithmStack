aa56b1a50848c96632b3c381e433a89dd6e8ccbd
Ssoco <dlscjs8646@gmail.com>
2022-02-18 21:07:36 +0900

    Create B5_24082_立方体_Cube.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_24082_立方体_Cube.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	// 24082 立方体 (Cube) 
+	// 수학
+	int x;
+	cin >> x;
+	cout << x * x * x;
+
+	return 0;
+}
