5b6993930913b9bcac3bb737df40faacb6a1195b
Ssoco <dlscjs8646@gmail.com>
2022-02-18 21:06:31 +0900

    Create B5_24078_余り_Remainder.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_24078_余り_Remainder.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	// 24078 余り (Remainder)
+	// 수학
+	int x;
+	cin >> x;
+	cout << x % 21;
+
+	
+	return 0;
+}
