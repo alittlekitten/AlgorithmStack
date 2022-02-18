5171b27b039f6d807c2c8a601d38982b572bd7b2
Ssoco <dlscjs8646@gmail.com>
2022-02-18 22:28:04 +0900

    Create B5_8871_Zadanie_próbne_2.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_8871_Zadanie_próbne_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	// 8871 Zadanie próbne 2
+	// 수학
+	int n;
+	cin >> n;
+	cout << 2 * (n + 1) << " " << 3 * (n + 1);
+
+	return 0;
+}
