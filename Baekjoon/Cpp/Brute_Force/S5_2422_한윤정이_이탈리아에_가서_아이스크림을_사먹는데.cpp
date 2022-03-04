f24b5ba7edae3dfa9c54bc48c2ea78b3f896e2e5
Ssoco <dlscjs8646@gmail.com>
2022-03-04 17:40:00 +0900

    Create S5_2422_한윤정이_이탈리아에_가서_아이스크림을_사먹는데.cpp

---
+++ Baekjoon/Cpp/Brute_Force/S5_2422_한윤정이_이탈리아에_가서_아이스크림을_사먹는데.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 2422 한윤정이 이탈리아에 가서 아이스크림을 사먹는데
+    // 브루트포스
+    int n, m, input1, input2;
+    int ans = 0;
+    bool arr[201][201] = { 0 };
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        cin >> input1 >> input2;
+        arr[input1][input2] = true;
+        arr[input2][input1] = true;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = i + 1; j <= n; ++j) {
+            if (arr[i][j] == false) {
+                for (int k = j + 1; k <= n; k++) {
+                    if (arr[i][k] == false && arr[j][k] == false) {
+                        ans++;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
