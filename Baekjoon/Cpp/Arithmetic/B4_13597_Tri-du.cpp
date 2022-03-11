dbcc9c35d30bc56d2a2f1823c80cb9a74fe13fed
Ssoco <dlscjs8646@gmail.com>
2022-03-12 04:06:50 +0900

    Create B4_13597_Tri-du.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_13597_Tri-du.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 13597 Tri-du
+    // 수학
+    int a, b;
+    cin >> a >> b;
+    cout << max(a, b);
+
+    return 0;
+}
