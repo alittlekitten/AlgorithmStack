6569aac27d7fa72bd36ba8c07aadd347e1e3f299
Ssoco <dlscjs8646@gmail.com>
2022-03-29 02:56:22 +0900

    Create G2_11442_홀수번째_피보나치_수의_합.cpp

---
+++ Baekjoon/Cpp/Devide_and_Conquer/G2_11442_홀수번째_피보나치_수의_합.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+using mat = vector<vector<ll>>;
+
+// 행렬 곱하는 함수
+mat mulMat(mat& a, mat& b) {
+    mat tmp(2, vector<ll>(2)); // 2차원 배열 생성
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                tmp[i][j] += a[i][k] * b[k][j];
+            }
+            tmp[i][j] %= 1000000007; // 곱한걸 1000000007로 나눠줌 (long long이기 때문에 한번에 나눠줘도 무방)
+        }
+    }
+    return tmp;
+}
+
+ll fibo(ll num) {
+    mat start = { {1,0},{0,1} }; // 단위행렬
+    mat target = { {1,1},{1,0} }; // 피보나치용 로직
+    while (num > 0) { // 지수를 2로 나눠주면서 빠르게 곱하기
+        if (num & 1) start = mulMat(target, start); // 나누다가 홀수인 경우 나머지 추가해주기
+        target = mulMat(target, target);
+        num /= 2;
+    }
+    return start[1][0];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+
+    // 11442 홀수번째 피보나치 수의 합
+    // 행렬곱 + 구현 (11444 피보나치 수 6 코드를 거의 그대로 가져다 쓰면 된다!)
+    // 1 0  단위 행렬에다가   1 1   2 * 2 행렬을 n만큼 중복해서 곱하면 첫 열의 첫 행에 n+1번째 피보나치 값, 두 번째 행에 n번재 피보나치 값이 등장!
+    // 0 1                    1 0
+
+    // 위 내용에 fibo(1) + fibo(3) + ... + fibo(n) = fibo(n+1) (n % 2 == 1) 이라는 것을 깨달으면 된다.
+    // (엄밀하게 따지면 fibo(n+1) + fibo(1) - fibo(2)인데, fibo(1)과 fibo(2)는 모두 1이므로 서로 상쇄된다.
+    // 직접 증명하자면 
+    // fibo(n) = fibo(n-1) + fibo(n-2)에서 fibo(n-2)를 fibo(n-3)과 fibo(n-4)의 합으로 쪼개고, fibo(n-4)를 fibo(n-5)와 fibo(n-6)의 합으로 쪼개고..를 반복하면 마지막까지 가서 0이 더해져서 홀수번째 합이 나온다.
+    // 해당 내용에 대한 자세한 증명 및 더 많은 자료는 http://www.koreascience.or.kr/article/JAKO200817639068191.pdf 를 참고
+    
+    ll n;
+    cin >> n;
+    if (n % 2) cout << fibo(n + 1); // 홀수번째면 fibo(n+1) 출력
+    else cout << fibo(n); // 짝수번째면 fibo(n) 출력
+
+    return 0;
+}
