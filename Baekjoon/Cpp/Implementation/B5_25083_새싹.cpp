0ac7f28434c6d6ea0f83f3fcc2536ed40064170c
Ssoco <dlscjs8646@gmail.com>
2022-04-30 01:39:41 +0900

    Create B5_25083_새싹.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_25083_새싹.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+
+    // 25083 새싹
+    // 구현
+
+    cout << "         ,r'\"7" << '\n';
+    cout << "r`-_   ,'  ,/" << '\n';
+    cout << " \\. \". L_r'" << '\n';
+    cout << "   `~\\/" << '\n';
+    cout << "      |" << '\n';
+    cout << "      |" << '\n';
+
+}
