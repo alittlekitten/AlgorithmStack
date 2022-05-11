d965ea97d808c4986d1e84af60fadf433ae8e3ae
Ssoco <dlscjs8646@gmail.com>
2022-05-11 18:18:56 +0900

    Create B3_11021_A+B-7.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B3_11021_A+B-7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+
+	  // 11021 A+B - 7
+    // 수학
+
+    int n, input1, input2;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> input1 >> input2;
+        cout << "Case #" << i + 1 << ": " << input1 + input2 << '\n';
+    }
+
+    return 0;
+}
