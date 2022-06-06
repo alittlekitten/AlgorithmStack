d6f642188c7ac4d8eb9362ce82c5d2cfa048604e
Ssoco <dlscjs8646@gmail.com>
2022-06-06 15:57:33 +0900

    Create B5_2743_단어_길이_재기.cpp

---
+++ Baekjoon/Cpp/String/B5_2743_단어_길이_재기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 2743 단어 길이 재기
+	// 문자열
+
+	string a;
+	cin >> a;
+	cout << a.size();
+
+	return 0;
+}
