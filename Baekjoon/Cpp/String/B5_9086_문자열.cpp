b3d00e6bd8b82db8bd8075e74c87a16a33333cf0
Ssoco <dlscjs8646@gmail.com>
2022-06-08 14:18:35 +0900

    Create B5_9086_문자열.cpp

---
+++ Baekjoon/Cpp/String/B5_9086_문자열.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    string s;
+    for (int i = 0; i < n; ++i){
+        cin >> s;
+        cout << s[0] << s[s.size()-1] << '\n';
+    }
+    
+    return 0;
+}
