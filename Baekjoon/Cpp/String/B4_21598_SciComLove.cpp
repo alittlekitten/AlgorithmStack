aa67df357ab3c027e31984e93389e16aa3d8bfb7
Ssoco <dlscjs8646@gmail.com>
2022-06-18 23:56:23 +0900

    Create B4_21598_SciComLove.cpp

---
+++ Baekjoon/Cpp/String/B4_21598_SciComLove.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 21598 SciComLove
+    // 문자열
+    int n;
+    cin >> n;
+    if (n == 1) cout << "SciComLove";
+    else cout << "SciComLove\nSciComLove";
+
+    return 0;
+}
