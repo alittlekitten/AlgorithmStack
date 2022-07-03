c74071da0a30d170dd8a16c4cd5f0e78c0979ddf
Ssoco <dlscjs8646@gmail.com>
2022-07-04 06:24:53 +0900

    Create B5_25311_UCPC에서_가장_쉬운_문제_번호는.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_25311_UCPC에서_가장_쉬운_문제_번호는.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 25311 UCPC에서 가장 쉬운 문제 번호는?
+    // 문자열
+
+    int n;
+    cin >> n;
+    cout << 'A';
+
+    return 0;
+}
