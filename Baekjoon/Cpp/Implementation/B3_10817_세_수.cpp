990b61adaec546756b9590a48e0ed84773237302
Ssoco <dlscjs8646@gmail.com>
2022-07-06 09:16:44 +0900

    Create B3_10817_세_수.cpp

---
+++ Baekjoon/Cpp/Implementation/B3_10817_세_수.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 10817 세 수
+    // 구현
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector<int> v;
+    v.push_back(a);
+    v.push_back(b);
+    v.push_back(c);
+    sort(v.begin(), v.end());
+    cout << v[1];
+
+    return 0;
+}
