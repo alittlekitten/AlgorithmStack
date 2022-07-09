2b2e538bb4a0206ef19b554d2d170c107ddf69c1
Ssoco <dlscjs8646@gmail.com>
2022-07-09 15:16:01 +0900

    Create B2_10987_모음의_개수.cpp

---
+++ Baekjoon/Cpp/String/B2_10987_모음의_개수.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 10987 모음의 개수
+    // 문자열
+    string s;
+    int ans = 0;
+    cin >> s;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') ++ans;
+    }
+
+    cout << ans;
+
+    return 0;
+}
