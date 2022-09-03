6a131e4ab58a352af83da3944380f064cdcbe9c8
Ssoco <dlscjs8646@gmail.com>
2022-09-03 09:51:12 +0900

    Create B4_15700_타일_채우기_4.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_15700_타일_채우기_4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+
+    // 15700 타일 채우기 4
+    // 수학
+    long long n, m;
+    cin >> n >> m;
+    cout << n * m / 2;
+
+    return 0;
+}
