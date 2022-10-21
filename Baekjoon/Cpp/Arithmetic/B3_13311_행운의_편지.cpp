7733af5b49c63ab667e879ad7826fb1c29a13652
Ssoco <dlscjs8646@gmail.com>
2022-10-21 20:22:18 +0900

    Create B3_13311_행운의_편지.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B3_13311_행운의_편지.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 13311 행운의 편지
+    // 수학
+    cout << -1;
+
+    return 0;
+}
