100ea2af8cc48a9b6d3d9abab58354ea07939dab
Ssoco <dlscjs8646@gmail.com>
2022-10-26 11:03:30 +0900

    Create B4_24723_녹색거탑.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_24723_녹색거탑.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 24723 녹색거탑
+    // 수학
+    int n;
+    cin >> n;
+    cout << (1 << n);
+
+    return 0;
+}
