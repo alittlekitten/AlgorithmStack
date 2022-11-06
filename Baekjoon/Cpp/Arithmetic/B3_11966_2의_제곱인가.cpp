91e57bb729c6b586ec44008e8473feb048c28268
Ssoco <dlscjs8646@gmail.com>
2022-11-06 17:06:47 +0900

    Create B3_11966_2의_제곱인가.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B3_11966_2의_제곱인가.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 11966 2의 제곱인가?
+    // 수학
+    int n;
+    cin >> n;
+    int target = 1;
+    if (n == 1) cout << 1;
+    while (target < n) target *= 2;
+    if (target == n) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
