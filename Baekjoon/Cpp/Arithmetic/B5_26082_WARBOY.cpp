509a27654eae31fc19ff2d76c7f9f5efdbabcc3e
Ssoco <dlscjs8646@gmail.com>
2022-11-28 20:59:56 +0900

    Create B5_26082_WARBOY.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_26082_WARBOY.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	// 26082 WARBOY
+	// 수학
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << b * c * 3 / a;
+
+	return 0;
+}
