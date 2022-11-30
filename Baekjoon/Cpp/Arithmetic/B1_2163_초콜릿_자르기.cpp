4e281ab0c5f20dc8dc7542e6d3f70ccd67789ee7
Ssoco <dlscjs8646@gmail.com>
2022-11-30 09:11:25 +0900

    Create B1_2163_초콜릿_자르기.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B1_2163_초콜릿_자르기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+
+    // 2163 초콜릿 자르기
+    // 수학
+    int n, m;
+    cin >> n >> m;
+    cout << n * m - 1;
+
+    return 0;
+}
