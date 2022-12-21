51069ae5410ec83ff4a6393692ab4c870cc53e6b
Ssoco <dlscjs8646@gmail.com>
2022-12-21 21:32:15 +0900

    Create B5_10189_Hook.cpp

---
+++ Baekjoon/Cpp/String/B5_10189_Hook.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 10189 Hook
+	// 문자열
+	cout << "#  # #### #### #  #\n#### #  # #  # # #\n#### #  # #  # # #\n#  # #### #### #  #";
+
+	return 0;
+}
