e593fe59f32b615c84a5fadd538b24deebeb7c67
Ssoco <dlscjs8646@gmail.com>
2022-12-28 14:04:46 +0900

    Create B5_26766_Serca.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_26766_Serca.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 26766 Serca
+	// 구현
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cout << " @@@   @@@\n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n";
+	}
+
+	return 0;
+}    
