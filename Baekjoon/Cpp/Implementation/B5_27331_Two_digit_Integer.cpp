fb9d3178bf8e861a57b1bd4684791e4c3e66a73e
Ssoco <dlscjs8646@gmail.com>
2023-01-29 15:16:59 +0900

    Create B5_27331_Two_digit_Integer.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_27331_Two_digit_Integer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 27331 2 桁の整数 (Two-digit Integer)
+	// 구현
+	int a, b;
+	cin >> a >> b;
+	cout << a << b;
+	
+	return 0;
+}
