e92e52501943170afec4bed1f332c3a28d2fcc8a
Ssoco <dlscjs8646@gmail.com>
2023-02-10 20:52:18 +0900

    Create B5_27389_Metronome.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_27389_Metronome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 27389 Metronome
+	// 수학
+	double n;
+	cin >> n;
+	cout << fixed;
+	cout.precision(2);
+	cout << n / 4;
+
+	return 0;
+}
