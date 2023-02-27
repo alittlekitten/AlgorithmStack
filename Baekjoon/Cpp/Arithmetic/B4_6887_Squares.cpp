f32d4ff4a3b0c251a0cc2bb05c04b97bb4c686d4
Ssoco <dlscjs8646@gmail.com>
2023-02-28 08:05:42 +0900

    Create B4_6887_Squares.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_6887_Squares.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 6887 Squares;
+	// 수학
+	int n;
+	cin >> n;
+	cout << "The largest square has side length " << (int)sqrt(n) << ".";
+
+	return 0;
+}
