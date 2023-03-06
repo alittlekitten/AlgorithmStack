30defcbd96d6f2ca7e3e42d45e6795008da3c297
Ssoco <dlscjs8646@gmail.com>
2023-03-07 07:42:45 +0900

    Create B4_14935_FA.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_14935_FA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 14935 FA
+	// 수학
+	int x;
+	cin >> x;
+	cout << "FA";
+
+	return 0;
+}
