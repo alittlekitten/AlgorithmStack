5314364101a798e07d68878ec3a66bb62caf0cac
Ssoco <dlscjs8646@gmail.com>
2023-04-06 08:12:14 +0900

    Create B3_27465_소수가_아닌_수.cpp

---
+++ Baekjoon/Cpp/Ad-Hoc/B3_27465_소수가_아닌_수.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 27465 소수가 아닌 수
+	// 애드혹
+	int n;
+	cin >> n;
+	cout << 1000000000;
+
+	return 0;
+}
