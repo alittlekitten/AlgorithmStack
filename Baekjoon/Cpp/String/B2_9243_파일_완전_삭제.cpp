6b144c2a6440c0411d3814395a3c668f8484f25b
Ssoco <dlscjs8646@gmail.com>
2023-06-25 13:40:05 +0900

    Update B2_9243_파일_완전_삭제.cpp

--- Baekjoon/Cpp/String/B2_9243_파일_완전_삭제.cpp
+++ Baekjoon/Cpp/String/B2_9243_파일_완전_삭제.cpp
@@ -8,28 +8,28 @@ int main() {
 	cout.tie(NULL);
 
 	// 9243 파일 완전 삭제
-  // 문자열
-  string s1, s2;
-  int n;
-  cin >> n >> s1 >> s2;
-  if (!(n % 2)) {
-    for (int i = 0; i < s1.size(); ++i) {
-        if (s1[i] != s2[i]) {
-            cout << "Deletion failed";
-            return 0;
+    // 문자열
+    string s1, s2;
+    int n;
+    cin >> n >> s1 >> s2;
+    if (!(n % 2)) {
+        for (int i = 0; i < s1.size(); ++i) {
+            if (s1[i] != s2[i]) {
+                cout << "Deletion failed";
+                return 0;
+            }
         }
+        cout << "Deletion succeeded";
     }
-    cout << "Deletion succeeded";
-  }
-  else {
-    for (int i = 0; i < s1.size(); ++i) {
-        if (s1[i] == s2[i]) {
-            cout << "Deletion failed";
-            return 0;
+    else {
+        for (int i = 0; i < s1.size(); ++i) {
+            if (s1[i] == s2[i]) {
+                cout << "Deletion failed";
+                return 0;
+            }
         }
+        cout << "Deletion succeeded";
     }
-    cout << "Deletion succeeded";
-  }
-
+   
 	return 0;
 }
