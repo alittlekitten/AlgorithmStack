31b5f854aeaef7a8b627e54f2fe6cc2db55c23a0
Ssoco <dlscjs8646@gmail.com>
2023-07-10 11:22:40 +0900

    Create B3_20944_팰린드롬_척화비.cpp

---
+++ Baekjoon/Cpp/Implementation/B3_20944_팰린드롬_척화비.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 20944 팰린드롬 척화비
+	// 구현
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i) cout << "a";
+
+	return 0;
+}
