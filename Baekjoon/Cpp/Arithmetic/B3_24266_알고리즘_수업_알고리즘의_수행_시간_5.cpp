7e5bd12d84ac6da05486eee7740832517ff430fb
Ssoco <dlscjs8646@gmail.com>
2023-09-03 11:56:04 +0900

    Create B3_24266_알고리즘_수업_알고리즘의_수행_시간_5.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B3_24266_알고리즘_수업_알고리즘의_수행_시간_5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 24266 알고리즘 수업 - 알고리즘의 수행 시간 5
+	// 수학
+	long long n;
+	cin >> n;
+	cout << n * n * n << '\n' << 3;
+
+	return 0;
+}
