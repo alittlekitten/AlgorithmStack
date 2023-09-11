03bd8cdcd41b3cfd220c10823526b194c67abf7a
Ssoco <dlscjs8646@gmail.com>
2023-09-12 06:31:52 +0900

    Create B5_2393_Rook.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_2393_Rook.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 2393 Rook
+	// 구현
+	cout << "  ___  ___  ___\n";
+	cout << "  | |__| |__| |\n";
+	cout << "  |           |\n";
+	cout << "   \\_________/\n";
+	cout << "    \\_______/\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |_____|\n";
+	cout << "  __/       \\__\n";
+	cout << " /             \\\n";
+	cout << "/_______________\\\n";
+
+	return 0;
+}
