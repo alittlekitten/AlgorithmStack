1aadd836744aa4c1048ac72e1d11303b4513f8b7
Ssoco <dlscjs8646@gmail.com>
2023-09-18 19:45:56 +0900

    Create B5_30030_스위트콘_가격_구하기.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_30030_스위트콘_가격_구하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 30030 스위트콘 가격 구하기
+	// 수학
+	int b;
+	cin >> b;
+	cout << b / 11 * 10;
+
+	return 0;
+}
