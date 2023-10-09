ef207df4b6da1966d91cd6db544e4ba4b8336af6
Ssoco <dlscjs8646@gmail.com>
2023-10-09 13:51:39 +0900

    Create B5_30328_Java_Warriors.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_30328_Java_Warriors.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 30328 Java Warriors
+	// 수학
+	int n;
+	cin >> n;
+	cout << n * 4000;
+
+	return 0;
+}
