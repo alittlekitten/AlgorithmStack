23962b5f925cca6ce177ec9279053fc32b98d7dd
Ssoco <dlscjs8646@gmail.com>
2024-03-16 15:51:20 +0900

    Create B5_31614_Minutes.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_31614_Minutes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 31614 分 (Minutes)
+	// 수학
+	int h, m;
+	cin >> h >> m;
+	cout << h * 60 + m;
+
+	return 0;
+}
