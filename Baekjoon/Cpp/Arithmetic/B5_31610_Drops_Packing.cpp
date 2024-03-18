21b4a0689126b99e25849ec515ac22e4f03d6f6a
Ssoco <dlscjs8646@gmail.com>
2024-03-19 08:44:57 +0900

    Create B5_31610_Drops_Packing.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_31610_Drops_Packing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(NULL);
+
+	// 31610 飴の袋詰め (Drops Packing)
+	// 수학
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << a * b + c;
+
+	return 0;
+}
