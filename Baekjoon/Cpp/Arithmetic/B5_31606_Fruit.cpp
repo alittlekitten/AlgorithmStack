adb961601703b5608b30bfef9cdcbf7208b4c123
Ssoco <dlscjs8646@gmail.com>
2024-03-21 08:40:03 +0900

    Create B5_31606_Fruit.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_31606_Fruit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(NULL);
+
+	// 31606 果物 (Fruit)
+	// 수학
+	int x, y;
+	cin >> x >> y;
+	cout << x + y + 3;
+
+	return 0;
+}
