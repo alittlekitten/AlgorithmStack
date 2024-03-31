88aae98b1e444ea27b004f2299248952b7188c03
Ssoco <dlscjs8646@gmail.com>
2024-03-31 19:39:13 +0900

    Create B4_31668_특별한_가지.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_31668_특별한_가지.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(NULL);
+
+	// 31668 특별한 가지
+	// 수학
+	int n, m, k;
+	cin >> n >> m >> k;
+	cout << m / n * k;
+
+	return 0;
+}
