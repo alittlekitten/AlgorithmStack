9b6a4446ab38d8b393fa8625ebe50c51c52c92a1
Ssoco <dlscjs8646@gmail.com>
2024-06-16 08:31:37 +0900

    Create S5_31780_불사조.cpp

---
+++ Baekjoon/Cpp/Ad-Hoc/S5_31780_불사조.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 31780 불사조
+	// 애드혹
+	int x, m;
+	cin >> x >> m;
+	cout << x * (m + 1);
+
+	return 0;
+}
