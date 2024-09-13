038b4efb169b7f5b432305958268c112f0ddaefa
Ssoco <dlscjs8646@gmail.com>
2024-09-13 22:51:41 +0900

    Create B4_32215_코드마스터_2024.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_32215_코드마스터_2024.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 32215 코드마스터 2024
+	// 수학
+	int n, m, k;
+	cin >> n >> m >> k;
+	cout << (k + 1) * m;
+	
+	return 0;
+}
