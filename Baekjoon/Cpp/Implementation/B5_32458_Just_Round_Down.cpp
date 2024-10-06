de2f505d212fa04a7926732a65dd79aec04d8f37
Ssoco <dlscjs8646@gmail.com>
2024-10-06 13:45:11 +0900

    Create B5_32458_Just_Round_Down.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_32458_Just_Round_Down.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 32458 Just Round Down
+	// 구현
+	int x;
+	cin >> x;
+	cout << x;
+
+	return 0;
+}
