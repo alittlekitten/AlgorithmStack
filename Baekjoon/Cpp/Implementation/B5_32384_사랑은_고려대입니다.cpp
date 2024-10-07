a7ca04f184bbcacdf047717f58f588f4dd533ff0
Ssoco <dlscjs8646@gmail.com>
2024-10-07 10:18:42 +0900

    Create B5_32384_사랑은_고려대입니다.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_32384_사랑은_고려대입니다.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 32384 사랑은 고려대입니다
+	// 구현
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i) cout << "LoveisKoreaUniversity ";
+
+	return 0;
+}
