ebcf4adbf6ede67a74824c59bdbff78dba9dc164
Ssoco <dlscjs8646@gmail.com>
2024-12-22 21:15:08 +0900

    Create B5_32951_AI_선도대학.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_32951_AI_선도대학.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 32951 AI 선도대학
+	// 구현
+	int n;
+	cin >> n;
+	cout << n - 2024;
+
+	return 0;
+}
