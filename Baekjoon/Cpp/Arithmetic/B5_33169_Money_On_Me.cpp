df245106adf89b4742733a5df38980b931ec6f73
Ssoco <dlscjs8646@gmail.com>
2025-01-14 08:41:54 +0900

    Create B5_33169_Money_On_Me.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_33169_Money_On_Me.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 33169 所持金 (Money On Me)
+	// 수학
+	int a, b;
+	cin >> a >> b;
+	cout << a * 1000 + b * 10000;
+
+	return 0;
+}
