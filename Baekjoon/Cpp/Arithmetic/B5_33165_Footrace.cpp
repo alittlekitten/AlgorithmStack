1750d281e7f129b378d5a42b7eefe2c8119f7732
Ssoco <dlscjs8646@gmail.com>
2025-01-15 09:09:44 +0900

    Create B5_33165_Footrace.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_33165_Footrace.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 33165 徒競走 (Footrace)
+	// 수학
+	int t, v;
+	cin >> t >> v;
+	cout << t * v;
+
+	return 0;
+}
