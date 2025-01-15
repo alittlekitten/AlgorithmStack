255932a2fa611c8b8f9f4dc25cbc7a3afd907865
Ssoco <dlscjs8646@gmail.com>
2025-01-16 08:45:23 +0900

    Create B5_33161_Pencils_2.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_33161_Pencils_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 33161 鉛筆 2 (Pencils 2)
+	// 수학
+	int a;
+	cin >> a;
+	cout << a / 5;
+
+	return 0;
+}
