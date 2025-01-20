7630ef666a1909757a112d8a318f38cb5884cf6c
Ssoco <dlscjs8646@gmail.com>
2025-01-20 11:00:18 +0900

    Create B4_33178_Micromasters.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_33178_Micromasters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 33178 Micromasters
+	// 수학
+	int n;
+	cin >> n;
+	cout << n / 10;
+
+	return 0;
+}
