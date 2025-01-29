bac5f35ddc421a342e2ab45840e9fc99362cf999
Ssoco <dlscjs8646@gmail.com>
2025-01-30 08:04:44 +0900

    Create B3_21553_암호_만들기.cpp

---
+++ Baekjoon/Cpp/Ad-Hoc/B3_21553_암호_만들기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 21553 암호 만들기
+	// 애드혹
+	string a, p;
+	cin >> a >> p;
+
+	string b = p;
+	cout << b;
+
+	return 0;
+}
