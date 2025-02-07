4438fd5cbf8a4918c86e5ae8547b72cf4b0bfb58
Ssoco <dlscjs8646@gmail.com>
2025-02-07 10:22:40 +0900

    Create B3_23530_Not_A_B.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B3_23530_Not_A_B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 23530 Not A + B
+	// 수학
+	int t, a, b;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		cin >> a >> b;
+		cout << 1 << '\n';
+	}
+
+	return 0;
+}
