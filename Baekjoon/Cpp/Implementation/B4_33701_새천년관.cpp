24aac9da436ac74591722a13a22c4b5bdaaccadc
Ssoco <dlscjs8646@gmail.com>
2025-04-13 14:03:28 +0900

    Create B4_33701_새천년관.cpp

---
+++ Baekjoon/Cpp/Implementation/B4_33701_새천년관.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 33701 새천년관
+	// 구현
+	cout << "nukugwan";
+
+	return 0;
+}
