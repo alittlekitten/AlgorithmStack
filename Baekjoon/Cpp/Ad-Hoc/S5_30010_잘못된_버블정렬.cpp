ff859db0fdeb7c4c5b58e3b54469464ee29384ff
Ssoco <dlscjs8646@gmail.com>
2025-05-20 07:50:53 +0900

    Create S5_30010_잘못된_버블정렬.cpp

---
+++ Baekjoon/Cpp/Ad-Hoc/S5_30010_잘못된_버블정렬.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 30010 잘못된 버블정렬
+	// 애드혹
+	int n;
+	cin >> n;
+
+	for (int i = n; i > 0; --i) cout << i << " ";
+
+	return 0;
+}
