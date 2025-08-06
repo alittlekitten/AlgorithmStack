100da410f984678da3aa7a999f8f81abf59bb233
Ssoco <dlscjs8646@gmail.com>
2025-08-07 07:19:55 +0900

    Create B3_33164_Dorayaki.cpp

---
+++ Baekjoon/Cpp/Brute_Force/B3_33164_Dorayaki.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 33164 どら焼き (Dorayaki)
+	// 브루트포스
+	int n, m, a, b;
+	int arr1[101] = { 0 };
+	int arr2[101] = { 0 };
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) cin >> arr1[i];
+	for (int i = 0; i < m; ++i) cin >> arr2[i];
+
+	int ans = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			ans += (arr1[i] + arr2[j]) * max(arr1[i], arr2[j]);
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
