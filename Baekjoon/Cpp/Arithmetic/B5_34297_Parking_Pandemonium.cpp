05b097a56a1c92ca4e9848b3a401d19a5677ff42
Ssoco <dlscjs8646@gmail.com>
2025-09-14 12:00:13 +0900

    Create B5_34297_Parking_Pandemonium.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_34297_Parking_Pandemonium.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34297 Parking Pandemonium
+	// 수학
+	int m, k, c;
+	cin >> m >> k >> c;
+	cout << m * c;
+
+	return 0;
+}
