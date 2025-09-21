a4b20afd44d0d1b29e00565b107d0dcbce3e61e7
Ssoco <dlscjs8646@gmail.com>
2025-09-21 16:55:12 +0900

    Create B5_34446_E_Days_Ore_Cart_Pull.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_34446_E_Days_Ore_Cart_Pull.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34446 E-Days Ore Cart Pull
+	// 수학
+	int m, n, t;
+	cin >> m >> n >> t;
+	cout << 2 * m;
+
+	return 0;
+}
