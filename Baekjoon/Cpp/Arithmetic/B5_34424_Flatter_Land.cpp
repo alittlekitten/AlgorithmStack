ca23366d8ad2f08ab6946331febdd3c4e9d95f3d
Ssoco <dlscjs8646@gmail.com>
2025-09-25 09:53:30 +0900

    Create B5_34424_Flatter_Land.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_34424_Flatter_Land.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34424 Flatter Land
+	// 수학
+	int n, x;
+	cin >> n >> x;
+	cout << (n - 1)* x;
+
+	return 0;
+}
