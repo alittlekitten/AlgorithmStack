8427ca784a3308de9877d9f8efea7834294fd6f6
Ssoco <dlscjs8646@gmail.com>
2025-09-29 08:22:54 +0900

    Create B5_34281_Class_Photo.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_34281_Class_Photo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34281 Class Photo
+	// 수학
+	int w, l;
+	cin >> w >> l;
+	cout << w * l;
+
+	return 0;
+}
