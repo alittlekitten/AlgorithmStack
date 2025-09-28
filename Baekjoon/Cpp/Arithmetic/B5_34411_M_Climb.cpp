893b9a208ec848c448f096ccd1f31342c9141835
Ssoco <dlscjs8646@gmail.com>
2025-09-28 16:26:43 +0900

    Create B5_34411_M_Climb.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_34411_M_Climb.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34411 M-Climb
+	// 수학
+	int m, n, y;
+	cin >> m >> n >> y;
+	cout << m * n;
+
+	return 0;
+}
