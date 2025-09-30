1f694949f84079841b00691d3d15698678c23a60
Ssoco <dlscjs8646@gmail.com>
2025-10-01 07:18:54 +0900

    Create B4_34509_2_4_6_8.cpp

---
+++ Baekjoon/Cpp/Brute_Force/B4_34509_2_4_6_8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34509 2, 4, 6, 8
+	// 브루트포스
+	cout << 42;
+
+	return 0;
+}
