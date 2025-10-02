33882f7ec5c5314f282ab29e2613797a50d32161
Ssoco <dlscjs8646@gmail.com>
2025-10-03 07:39:56 +0900

    Create B5_34443_Hack_a_Holics.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_34443_Hack_a_Holics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34443 Hack-a-Holics
+	// 수학
+	int n, c, p;
+	cin >> n >> c >> p;
+	cout << n * p;
+
+	return 0;
+}
