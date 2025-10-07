521e19da7eb1c3e6d7e591bd48c023996687899a
Ssoco <dlscjs8646@gmail.com>
2025-10-07 13:41:05 +0900

    Create B4_34380_Blasters_Dash.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_34380_Blasters_Dash.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34380 Blaster's Dash
+	// 수학
+	int m, v, n, k;
+	cin >> m >> v >> n >> k;
+	cout << 40 + n * 2;
+
+	return 0;
+}
