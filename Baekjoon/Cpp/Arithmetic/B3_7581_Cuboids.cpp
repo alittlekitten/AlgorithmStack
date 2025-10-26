77dee01077784f1050446b02513a4cf08767226c
Ssoco <dlscjs8646@gmail.com>
2025-10-26 20:28:41 +0900

    Create B3_7581_Cuboids.cpp
    Implement a program to calculate dimensions of cuboids based on given inputs.

---
+++ Baekjoon/Cpp/Arithmetic/B3_7581_Cuboids.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 7581 Cuboids
+	// 수학
+	while (1) {
+		int l, w, h, v;
+		cin >> l >> w >> h >> v;
+
+		if (l == 0 && w == 0 && h == 0 && v == 0) break;
+		if (l == 0) l = v / (w * h);
+		if (w == 0) w = v / (l * h);
+		if (h == 0) h = v / (l * w);
+		if (v == 0) v = l * w * h;
+		cout << l << " " << w << " " << h << " " << v << '\n';
+	}
+
+	return 0;
+}
