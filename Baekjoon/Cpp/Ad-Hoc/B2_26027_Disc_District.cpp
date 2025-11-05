001c35864635ed189b577ed5a42df81a54c0f5b1
Ssoco <dlscjs8646@gmail.com>
2025-11-06 07:30:48 +0900

    Create B2_26027_Disc_District.cpp

---
+++ Baekjoon/Cpp/Ad-Hoc/B2_26027_Disc_District.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 26027 Disc District
+	// 애드 혹
+	int r;
+	cin >> r;
+	cout << 1 << ' ' << r;
+
+	return 0;
+}
