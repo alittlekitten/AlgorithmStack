b2aebd70dd152cd92fe7b23b1fb3018066e805e0
Ssoco <dlscjs8646@gmail.com>
2025-11-29 09:04:55 +0900

    Create B3_34759_얼룩말과_사자.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B3_34759_얼룩말과_사자.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34759 얼룩말과 사자
+	// 수학
+	long long n;
+	cin >> n;
+	cout << n * 2;
+
+	return 0;
+}
