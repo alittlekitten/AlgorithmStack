47bfc9334d6881c6d37cbd6bb8fe1f9deae4b72a
Ssoco <dlscjs8646@gmail.com>
2025-12-03 07:22:16 +0900

    Create B5_34721_역사를_걸으면_동국이_보이고.cpp

---
+++ Baekjoon/Cpp/Implementation/B5_34721_역사를_걸으면_동국이_보이고.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34721 역사를 걸으면 동국이 보이고
+	// 구현
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cout << "I love DGU\n";
+	}
+
+	return 0;
+}
