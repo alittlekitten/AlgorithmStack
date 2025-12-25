2300bfc18f6d345acd6c8e07aa28f9c93c0e7461
Ssoco <dlscjs8646@gmail.com>
2025-12-25 18:35:33 +0900

    Create B5_34945_채플이_너무해.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B5_34945_채플이_너무해.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34945 채플이 너무해
+	// 수학
+	int x;
+	cin >> x;
+	if (x >= 6) cout << "Success!";
+	else cout << "Oh My God!";
+
+	return 0;
+}
