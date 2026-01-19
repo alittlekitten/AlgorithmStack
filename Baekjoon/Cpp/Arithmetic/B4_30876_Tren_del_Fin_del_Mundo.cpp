03decd2805cd1dad6c6df37450e7302702f974fb
Ssoco <dlscjs8646@gmail.com>
2026-01-19 20:52:27 +0900

    Create B4_30876_Tren_del_Fin_del_Mundo.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_30876_Tren_del_Fin_del_Mundo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 30876 Tren del Fin del Mundo
+	// 수학
+    int n, x, y;
+
+    cin >> n;
+    int xx = 1001;
+    int yy = 1001;
+    for (int i = 0; i < n; ++i) {
+        cin >> x >> y;
+        if (y < yy) {
+            xx = x;
+            yy = y;
+        }
+    }
+    cout << xx << " " << yy;
+
+	return 0;
+}
