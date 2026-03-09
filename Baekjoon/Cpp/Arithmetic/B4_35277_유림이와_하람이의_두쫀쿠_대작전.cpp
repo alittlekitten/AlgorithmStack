52045d0ad0d9dd846d12b0a47ae0140bfe8c554e
Ssoco <dlscjs8646@gmail.com>
2026-03-09 12:15:45 +0900

    Create B4_35277_유림이와_하람이의_두쫀쿠_대작전.cpp

---
+++ Baekjoon/Cpp/Arithmetic/B4_35277_유림이와_하람이의_두쫀쿠_대작전.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 35277 유림이와 하람이의 두쫀쿠 대작전
+	// 수학
+	int n;
+	cin >> n;
+	cout << n / 2000;
+
+	return 0;
+}
