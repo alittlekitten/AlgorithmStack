3642cfb47cdb8f1e799bc91ced0a5cf5501976d0
Ssoco <dlscjs8646@gmail.com>
2026-03-19 07:28:40 +0900

    Create B2_34667_가희와_철도역.cpp

---
+++ Baekjoon/Cpp/String/B2_34667_가희와_철도역.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// 34667 가희와 철도역
+	// 문자열
+	string s;
+	cin >> s >> s;
+
+	cout << s[0] + s;
+
+	return 0;
+}
