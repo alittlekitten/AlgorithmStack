71b0e98bf490f3cef1a8018d315044a4f44f43aa
Ssoco <dlscjs8646@gmail.com>
2026-05-03 02:58:17 +0900

    Create Easy_26_Remove_Duplicates_from_Sorted_Array.cpp

---
+++ LeetCode/Cpp/Easy_26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 1) return nums.size();
+        
+        int idx = 1;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] != nums[i + 1]) nums[idx++] = nums[i + 1];
+        }
+        return idx;
+    }
+};
