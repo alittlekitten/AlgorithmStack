61261a65491fa6af791584bdba3d5ef366502ea5
Ssoco <dlscjs8646@gmail.com>
2026-05-15 10:52:34 +0900

    Create Easy_392_Is_Subsequence.cpp

---
+++ LeetCode/Cpp/Easy_392_Is_Subsequence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int idx = 0;
+        for (char c : t) {
+            if (s[idx] == c) ++idx;
+        }
+
+        return idx == s.size();
+    }
+};
