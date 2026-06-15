b2d64a05fc553f120b4a38923ac360e6ec0c070a
Ssoco <dlscjs8646@gmail.com>
2026-06-16 01:08:43 +0900

    Create Easy_704_Binary_Search.cpp

---
+++ LeetCode/Cpp/Easy_704_Binary_Search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int h = nums.size() - 1;
+        
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (nums[m] == target) return m;
+            
+            if (nums[m] < target) {
+                l = m + 1;
+            }
+            else { 
+                h = m - 1;
+            }
+        }
+        return -1;
+    }
+};
