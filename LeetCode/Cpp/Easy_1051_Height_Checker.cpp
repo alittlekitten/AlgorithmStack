5ae5096662aac038876982cabe2141410ff584c5
Ssoco <dlscjs8646@gmail.com>
2026-06-18 10:07:05 +0900

    Create Easy_1051_Height_Checker.cpp

---
+++ LeetCode/Cpp/Easy_1051_Height_Checker.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> v = heights;
+        sort(v.begin(), v.end());
+
+        int ans = 0;
+        for(int i = 0; i < heights.size(); ++i) {
+            if (heights[i] != v[i]) ++ans;
+        }
+        return ans;
+    }
+};
