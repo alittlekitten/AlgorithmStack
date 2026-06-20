d2d5e2f60b8f686163412a88ded87f7fa844aedd
Ssoco <dlscjs8646@gmail.com>
2026-06-20 14:21:08 +0900

    Create Easy_14_Longest_Common_Prefix.cpp

---
+++ LeetCode/Cpp/Easy_14_Longest_Common_Prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(), strs.end());
+        string ans = "";
+        string f = strs[0];
+        string l = strs[strs.size() - 1];
+        for (int i = 0; i < strs[0].size(); ++i) { 
+            if (f[i] == l[i]) ans += f[i];
+            else break;
+        }
+        return res;
+    }
+};
