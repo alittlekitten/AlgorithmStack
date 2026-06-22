51f4aee17d7af959fe4c0d858199f7fd29fdd752
Ssoco <dlscjs8646@gmail.com>
2026-06-23 02:13:24 +0900

    Create Medium_20_Valid_Parentheses.cpp

---
+++ LeetCode/Cpp/Medium_20_Valid_Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto c : s) {
+            if(c == '(' || c == '{' || c == '[') st.push(c);
+            else {
+                if(st.empty()) return false;
+                int prev = st.top();
+                st.pop();
+                if((c == ')' && prev == '(') || 
+                (c == '}' && prev == '{') || 
+                (c == ']' && prev == '[')) continue;
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};
