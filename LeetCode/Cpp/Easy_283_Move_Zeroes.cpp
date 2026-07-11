eab9e9eb2f6afda47a95f1a51fe2cce2791c726b
Ssoco <dlscjs8646@gmail.com>
2026-07-12 00:25:58 +0900

    Create Easy_283_Move_Zeroes.cpp

---
+++ LeetCode/Cpp/Easy_283_Move_Zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int idx = 0;
+        for (int i = 0; i < nums.size(); ++i){
+            if (nums[i]){
+                swap(nums[i], nums[idx]);
+                ++idx;
+            }
+        }
+    }
+};
