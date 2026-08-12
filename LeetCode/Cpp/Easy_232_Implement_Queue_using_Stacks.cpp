15464f2e245b5105085b0a9c692648b78a743b1f
Ssoco <dlscjs8646@gmail.com>
2026-08-13 00:15:22 +0900

    Create Easy_232_Implement_Queue_using_Stacks.cpp

---
+++ LeetCode/Cpp/Easy_232_Implement_Queue_using_Stacks.cpp
@@ -0,0 +1,48 @@
+class MyQueue {
+public:
+    stack<int> st1;
+    stack<int> st2;
+
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        while (st1.size() > 1) {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int t = st1.top();
+        st1.pop();
+        while (st2.size()) {
+            st1.push(st2.top());
+            st2.pop();
+        }
+        return t;
+    }
+    
+    int peek() {
+        while (st1.size() > 1) {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int t = st1.top();
+        while (st2.size()) {
+            st1.push(st2.top());
+            st2.pop();
+        }
+        return t;
+    }
+    bool empty() {
+        return st1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
