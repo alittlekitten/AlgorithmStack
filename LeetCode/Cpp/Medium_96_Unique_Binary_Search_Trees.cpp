34101155ed134506d774174771f4b8e936eef1e7
Ssoco <dlscjs8646@gmail.com>
2026-08-21 00:15:27 +0900

    Create Medium_96_Unique_Binary_Search_Trees.cpp

---
+++ LeetCode/Cpp/Medium_96_Unique_Binary_Search_Trees.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        int dp[20] = { 0 };
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; ++i) {
+            for (int j = 1; j < i + 1; ++j) {
+                dp[i] = dp[i] + dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
