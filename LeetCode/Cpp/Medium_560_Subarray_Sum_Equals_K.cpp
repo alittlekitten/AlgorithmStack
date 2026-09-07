ec81941fe0e4c8dd0b45e3564f378b6a79ff8a63
Ssoco <dlscjs8646@gmail.com>
2026-09-08 06:50:35 +0900

    Create Medium_560_Subarray_Sum_Equals_K.cpp

---
+++ LeetCode/Cpp/Medium_560_Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int> m;
+        int sum = 0;
+        int ans = 0;
+        m[0] = 1;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            if (m.find(sum - k) != m.end()) ans += m[sum - k];
+            ++m[sum];
+        }
+
+        return ans;
+    }
+};
